16de4a078ef430517b8778df6793f88017c3dba5
Ofri Gal <ofri0101@gmail.com>
2025-05-26 11:27:02 +0300

    renamed agreementDTO to contract DTO

--- dev/DTOs/ContractDTO.java
+++ dev/DTOs/ContractDTO.java
@@ -7,15 +7,15 @@ import Domain.DeliveryMethod;
 import Domain.DiscountDL;
 import Domain.Item;
 
-public class AgreementDTO {
-    private int agreementID;
+public class ContractDTO {
+    private int contractID;
     private Map<Item, Integer> itemCatalog;
     private List<DiscountDL> billOfQuantities;
     private DeliveryMethod deliveryMethod;
 
-    public AgreementDTO(int agreementID, Map<Item, Integer> itemCatalog, List<DiscountDL> billOfQuantities,
+    public ContractDTO(int contractID, Map<Item, Integer> itemCatalog, List<DiscountDL> billOfQuantities,
             DeliveryMethod deliveryMethod) {
-        this.agreementID = agreementID;
+        this.contractID = contractID;
         this.itemCatalog = itemCatalog;
         this.billOfQuantities = billOfQuantities;
         this.deliveryMethod = deliveryMethod;
